0be858c4dc567292675a261131155e51c51e7116
CameronLumpkinATL <camlump@yahoo.com>
2020-07-16 19:39:09 -0400

    changed media query from 700 to 800 min-width

--- main.js
+++ main.js
@@ -9,17 +9,44 @@ window.addEventListener("scroll", function(){
 
 });
 
-$(document).on('click', 'ul li', function(){
-    $(this).addClass('active').siblings().removeClass('active')
+
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+
+    $('.section').each(function(){
+        let target = $(this).offset().top;
+        let id = $(this).attr('id');
+
+        if(position >= target) {
+            $('#topNav > ul > li > a').removeClass('active');
+                $('#topNav > ul > li > a [href=#' + id + ']').addClass('active') 
+        } 
+    })
+
+
+
 });
 
-//scroll function
-$('.navbar a').on('click', function(e){
-    if(this.hash !== '') {
-        e.preventDefault();
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800);
-    }
-});
+
+
+
+
+
+
+// $(document).on('click', 'ul li', function(){
+//     $(this).addClass('active').siblings().removeClass('active')
+// });
+
+// //scroll function
+// $('.navbar a').on('click', function(e){
+//     if(this.hash !== '') {
+//         e.preventDefault();
+
+//         $('html, body').animate({
+//             scrollTop: $(hash).offset().top
+//         }, 800);
+//     }
+// });
+
+
